4fd0c8b30a46eb2a9c9355662b3fdfdfeef5f94b
sbojorge <sara.bojorge@gmail.com>
2023-05-19 16:21:27 +0200

    manipulate the DOM with JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
 document.getElementById("greeting").innerHTML = 'Still 30 minutes of fun';
+document.getElementById("greeting").style.color = 'blue';
+document.getElementById("text").style.display = 'block';
+
+function myFunction() {
+    document.getElementById('picture').src = 'assets/images/160429.webp';
+}
+
+myFunction();
 
