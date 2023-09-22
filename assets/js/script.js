b30939c1d9ec7ac3333be864feb0e96ff8986508
sbojorge <sara.bojorge@gmail.com>
2023-09-22 12:26:20 +0200

    Practice if-else and switch case

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-document.getElementById("greeting").innerHTML = 'Still 30 minutes of fun';
-document.getElementById("greeting").style.color = 'blue';
-document.getElementById("text").style.display = 'block';
+document.getElementById("greeting").innerHTML = "Still 30 minutes of fun";
+document.getElementById("greeting").style.color = "blue";
+document.getElementById("text").style.display = "block";
 
 function myFunction() {
-    document.getElementById('picture').src = 'assets/images/160429.webp';
+  document.getElementById("picture").src = "assets/images/160429.webp";
 }
 
 myFunction();
@@ -61,32 +61,143 @@ myFunction();
 
 // console.log(newFullName.slice(0,4) + "." + newFullName.slice(5,13) + "@" + domain + "." + tld );
 
-console.log(Number("123"));
-console.log(Number(null));
-console.log(Number(true));
-console.log(Number("hello"));
-console.log(isNaN("hi"));
-console.log(isFinite(1.8e308));
-console.log(Number.isInteger(123));
-console.log(parseFloat("345.9"));
-console.log(parseInt(98.98));
-console.log(345.5.toFixed(2));
-console.log(typeof(345.5.toString()));
+// console.log(Number("123"));
+// console.log(Number(null));
+// console.log(Number(true));
+// console.log(Number("hello"));
+// console.log(isNaN("hi"));
+// console.log(isFinite(1.8e308));
+// console.log(Number.isInteger(123));
+// console.log(parseFloat("345.9"));
+// console.log(parseInt(98.98));
+// console.log(345.5.toFixed(2));
+// console.log(typeof(345.5.toString()));
 
-let y;
-console.log(y);
+// let y;
+// console.log(y);
 
-console.log(isNaN("56"));
+// console.log(isNaN("56"));
 
-let passport = true;
-let ticket = true;
-let weapons = false;
-let a = 10;
-let b = 5;
+// let passport = true;
+// let ticket = true;
+// let weapons = false;
+// let a = 10;
+// let b = 5;
 
-let either = a > 10 || b <= 5;
-console.log(either);
+// let either = a > 10 || b <= 5;
+// console.log(either);
 
-let getOnFlight = passport && ticket && !weapons;
-console.log(getOnFlight);
+// let getOnFlight = passport && ticket && !weapons;
+// console.log(getOnFlight);
 
+// let userLoggedIn = true;
+// if (userLoggedIn) {
+//   alert("Welcome");
+// } else {
+//   alert("Please log in");
+// }
+
+let a = 1;
+let b = 2;
+let firstGreater;
+
+if (a > b) {
+  firstGreater = true;
+} else {
+  firstGreater = false;
+}
+console.log(firstGreater);
+
+let price;
+let memberType = "pro";
+
+switch (memberType) {
+    case "basic":
+        price = 40;
+        break;
+    case "pro":
+        price = 50;
+        break;
+    case "gold":
+        price = 0;
+        break;
+    default:
+        price = 0
+
+}
+console.log(`price is ${price}`);
+
+let x = 5;
+let y = 4;
+
+let result = x < y ? "x is smaller" : "x is greater";
+console.log(result);
+
+let memberShip = "trial";
+let newPrice;
+
+if (memberShip === "basic"){
+    newPrice = 30;
+} else if (memberShip === "pro"){
+    newPrice = 35;
+} else if (memberShip === "elite"){
+    newPrice = 40;
+} else {
+    newPrice = 0;
+}
+console.log(newPrice)
+
+let age = 23;
+let newResult;
+
+if (age > 21){
+    newResult = "drink whiskey";
+} else if (age > 18) {
+    newResult = "drink beer";
+} else if (age > 12) {
+    newResult = "drink coca cola";
+} else {
+    newResult = "drink water";
+}
+console.log(newResult)
+
+function findPrice(memberStatus) {
+    switch (memberStatus) {
+      case 0:
+        return 54;
+      case 1:
+        return 34;
+      case 2:
+        return 12;
+      case 3:
+        return 78;
+      case 4:
+        return 98;
+      case 5:
+        return 4958;
+      case 6:
+        return 344;
+      default:
+        return 'Invalid member status';
+    }
+  }
+  let day = findPrice(6);
+  console.log(day);
+
+  let challengeResult = "";
+  let errorType = "page";
+
+  switch (errorType) {
+    case "username":
+        challengeResult = "That username is incorrect, please try again.";
+        break;
+    case "password":
+        challengeResult = "Incorrect password, please try again.";
+        break;
+    case "page":
+        challengeResult = "Sorry this page doesn't exist.";
+        break;
+    default:
+        challengeResult = "Error message unknown";
+  }
+  console.log(challengeResult);
