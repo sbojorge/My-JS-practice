64894761e754d25b81571fda8b377f286e9d0a2f
sbojorge <sara.bojorge@gmail.com>
2023-09-25 11:13:42 +0200

    Practice iterations

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,122 +97,172 @@ myFunction();
 //   alert("Please log in");
 // }
 
-let a = 1;
-let b = 2;
-let firstGreater;
+// let a = 1;
+// let b = 2;
+// let firstGreater;
 
-if (a > b) {
-  firstGreater = true;
-} else {
-  firstGreater = false;
+// if (a > b) {
+//   firstGreater = true;
+// } else {
+//   firstGreater = false;
+// }
+// console.log(firstGreater);
+
+// let price;
+// let memberType = "pro";
+
+// switch (memberType) {
+//     case "basic":
+//         price = 40;
+//         break;
+//     case "pro":
+//         price = 50;
+//         break;
+//     case "gold":
+//         price = 0;
+//         break;
+//     default:
+//         price = 0
+
+// }
+// console.log(`price is ${price}`);
+
+// let x = 5;
+// let y = 4;
+
+// let result = x < y ? "x is smaller" : "x is greater";
+// console.log(result);
+
+// let memberShip = "trial";
+// let newPrice;
+
+// if (memberShip === "basic"){
+//     newPrice = 30;
+// } else if (memberShip === "pro"){
+//     newPrice = 35;
+// } else if (memberShip === "elite"){
+//     newPrice = 40;
+// } else {
+//     newPrice = 0;
+// }
+// console.log(newPrice)
+
+// let age = 23;
+// let newResult;
+
+// if (age > 21){
+//     newResult = "drink whiskey";
+// } else if (age > 18) {
+//     newResult = "drink beer";
+// } else if (age > 12) {
+//     newResult = "drink coca cola";
+// } else {
+//     newResult = "drink water";
+// }
+// console.log(newResult)
+
+// function findPrice(memberStatus) {
+//     switch (memberStatus) {
+//       case 0:
+//         return 54;
+//       case 1:
+//         return 34;
+//       case 2:
+//         return 12;
+//       case 3:
+//         return 78;
+//       case 4:
+//         return 98;
+//       case 5:
+//         return 4958;
+//       case 6:
+//         return 344;
+//       default:
+//         return 'Invalid member status';
+//     }
+//   }
+//   let day = findPrice(6);
+//   console.log(day);
+
+//   let challengeResult = "";
+//   let errorType = "page";
+
+//   switch (errorType) {
+//     case "username":
+//         challengeResult = "That username is incorrect, please try again.";
+//         break;
+//     case "password":
+//         challengeResult = "Incorrect password, please try again.";
+//         break;
+//     case "page":
+//         challengeResult = "Sorry this page doesn't exist.";
+//         break;
+//     default:
+//         challengeResult = "Error message unknown";
+//   }
+//   console.log(challengeResult);
+
+//   let user = true;
+//   let emailVerified = false;
+
+//   if (user) {
+//     if (emailVerified) {
+//       console.log("Welcome to our web-site");
+//       console.log(emailVerified);
+//     } else {
+//       console.log("Please verify your email");
+//     }
+//   } else {
+//     console.log("You need to be a user to visit this page");
+//   }
+
+let fruits = ["apples", "kiwis", "bananas", "cherries"];
+let numberOfFruits = fruits.length;
+
+let i = 0;
+while (i < numberOfFruits) {
+  console.log(fruits[i] +  " are delicious");
+  i++;
 }
-console.log(firstGreater);
 
-let price;
-let memberType = "pro";
+console.log("I love fruits!");
 
-switch (memberType) {
-    case "basic":
-        price = 40;
-        break;
-    case "pro":
-        price = 50;
-        break;
-    case "gold":
-        price = 0;
-        break;
-    default:
-        price = 0
-
+let x = 0;
+while (x <= 10) {
+  console.log(x);
+  x++;
 }
-console.log(`price is ${price}`);
 
-let x = 5;
-let y = 4;
+let attemptedUploads = 0;
 
-let result = x < y ? "x is smaller" : "x is greater";
-console.log(result);
+do {
+  console.log("Attempting upload...");
+  attemptedUploads++;
+} while (attemptedUploads < 5);
 
-let memberShip = "trial";
-let newPrice;
+console.log("Upload attempts exceeded maximum");
 
-if (memberShip === "basic"){
-    newPrice = 30;
-} else if (memberShip === "pro"){
-    newPrice = 35;
-} else if (memberShip === "elite"){
-    newPrice = 40;
-} else {
-    newPrice = 0;
-}
-console.log(newPrice)
-
-let age = 23;
-let newResult;
-
-if (age > 21){
-    newResult = "drink whiskey";
-} else if (age > 18) {
-    newResult = "drink beer";
-} else if (age > 12) {
-    newResult = "drink coca cola";
-} else {
-    newResult = "drink water";
-}
-console.log(newResult)
-
-function findPrice(memberStatus) {
-    switch (memberStatus) {
-      case 0:
-        return 54;
-      case 1:
-        return 34;
-      case 2:
-        return 12;
-      case 3:
-        return 78;
-      case 4:
-        return 98;
-      case 5:
-        return 4958;
-      case 6:
-        return 344;
-      default:
-        return 'Invalid member status';
-    }
+let y = 0;
+while (y < 1000) {
+  if (y === 3) {
+    console.log("breaking");
+    break;
   }
-  let day = findPrice(6);
-  console.log(day);
+  console.log(y);
+  y++;
+}
+console.log("loop has been broken");
 
-  let challengeResult = "";
-  let errorType = "page";
-
-  switch (errorType) {
-    case "username":
-        challengeResult = "That username is incorrect, please try again.";
-        break;
-    case "password":
-        challengeResult = "Incorrect password, please try again.";
-        break;
-    case "page":
-        challengeResult = "Sorry this page doesn't exist.";
-        break;
-    default:
-        challengeResult = "Error message unknown";
+for (let z = 0; z <=6; z++) {
+  if (z % 2 !== 0){
+    continue;
   }
-  console.log(challengeResult);
+  console.log(z);
+}
+console.log("Loop complete");
 
-  let user = true;
-  let emailVerified = false;
-
-  if (user) {
-    if (emailVerified) {
-      console.log("Welcome to our web-site");
-      console.log(emailVerified);
-    } else {
-      console.log("Please verify your email");
-    }
-  } else {
-    console.log("You need to be a user to visit this page");
+for(let a = 0; a <=3; a++) {
+  for(let b = 200; b <= 203; b++){
+    console.log("outer loop a:", a, "inner loop b:", b);
   }
-
+}
