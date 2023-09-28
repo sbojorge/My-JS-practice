cffa95aeee1d440ae626c0e9173d04bf4ecb411f
sbojorge <sara.bojorge@gmail.com>
2023-09-28 10:28:23 +0200

    Practice targetting elements by id

---
+++ assets/js/id-practice.js
@@ -0,0 +1,12 @@
+let firstHeading = document.getElementById("first-heading");
+let secondHeading = document.getElementById("second-heading");
+let thirdHeading = document.getElementById("third-heading");
+let fourthHeading = document.getElementById("fourth-heading");
+
+console.log(firstHeading.innerHTML);
+console.log(secondHeading.textContent);
+console.log(thirdHeading.innerText);
+console.log(fourthHeading.innerHTML);
+
+let special = document.getElementById("special");
+console.log(special.innerHTML);
