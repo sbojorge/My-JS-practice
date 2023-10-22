3309a082c12fff8fa83dcb3cfb0e33453f8077a3
sbojorge <sara.bojorge@gmail.com>
2023-10-22 15:59:46 +0200

    Add image

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,9 @@ document.getElementById("greeting").innerHTML = "Still 30 minutes of fun";
 document.getElementById("greeting").style.color = "blue";
 document.getElementById("text").style.display = "block";
 
+
 function myFunction() {
-  document.write("this is my test");
+  console.log("hi");
 }
 
 myFunction();
